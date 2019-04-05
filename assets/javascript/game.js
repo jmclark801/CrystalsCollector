610a13584e756bf27223dfb1c464fc3850f31315
NunyaBizness <jmclark801@yahoo.com>
2019-04-04 19:28:31 -0600

    Update win/loss rules

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,16 @@
 var randomScore = 0;
 var userScore = 0;
 var gemNumbers = [];
+var wins = 0;
+var losses = 0;
 // On click of start button, add the randomly generated number from 19 - 120
 // Add that number to the Target Score div
 $('#start-button').on('click', function(){
   gemNumbers = [];
   randomScore = Math.ceil( (Math.random() * 100) + 18)
   $('#target-score').text(randomScore);
-  console.log("Random Score: ", randomScore);
   for (var i = 1; i < 5; i++) {
-    // Ensure every button has a unique number 
+    // Ensure every button has a unique number 2-13 since if the #1 is picked, the user can win too easily.
     var randomGemNumber = Math.floor( (Math.random() * 12) + 2) 
     if (gemNumbers.indexOf(randomGemNumber) === -1) {
       gemNumbers.push(randomGemNumber)
@@ -19,29 +20,37 @@ $('#start-button').on('click', function(){
     } 
     $('#gem-' + i).val(gemNumbers[i-1])
   }
-  console.log("Value of gem-1: ", $('#gem-1').val());
-  console.log("Value of gem-2: ", $('#gem-2').val());
-  console.log("Value of gem-3: ", $('#gem-3').val());
-  console.log("Value of gem-4: ", $('#gem-4').val());
 });
-var number = 1;
+
 // On click of a gem, add the value of that gem to the user's total
-$('#gem-' + number).on('click', function(){
-  // Update the userScore if there is a value for the gem
-  if ($(this).val()) {
-    userScore += parseInt($(this).val());
-    $('#user-score').text(userScore);
-    evaluateScore();
-  }
-});
+for(var i=0; i <= 4; i++){
+  $('#gem-' + i).on('click', function(){
+    if ($(this).val()) {
+      userScore += parseInt($(this).val());
+      $('#user-score').text(userScore);
+      evaluateScore();
+    }
+  });
+}
 
 // Determine if Score Matches
   function evaluateScore(){
    if (userScore === randomScore){
-     console.log("You Win!");
+    wins += 1;
+    $('#wins').text('Wins: ' + wins);
+    $('#status').text('Congrats, You Won!').attr("id","status-win")
+
    } else if (userScore > randomScore){
-     console.log("You went over. So sad, you are a loser and no one likes you.");
-   } else {
+    losses +=1;
+    $('#losses').text('Losses: ' + losses);
+    $('#status').text('You Went Over.  Sorry, You Lost!').attr("id","status-lose")
+    } else {
      console.log("Keep going!");
    }
   }
+
+  //To-Do
+  // 1. Change Start Button to 'Play Again' after initial click of start button
+  // 2. Clear all variables on 'Play Again' - reset function
+  // 3. Clean up presentation
+  // 4. Add logging of numbers so 'win' scenario can be tested.
