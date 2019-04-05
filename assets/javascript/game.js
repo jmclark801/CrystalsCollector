70fa9ca5a9ac1ebfb05d62f3558ac900a9bb3aaf
NunyaBizness <jmclark801@yahoo.com>
2019-04-05 13:37:59 -0600

    Add end game status and ability to reset the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,28 +4,38 @@ var userScore = 0;
 var gemNumbers = [];
 var wins = 0;
 var losses = 0;
-// On click of start button, add the randomly generated number from 19 - 120
-// Add that number to the Target Score div
-$('#start-button').on('click', function(){
+
+// On click of start button:
+// Variables are reset and classes, text are updated to visually reset the game
+// Randomly generated target score is set from 19 - 120
+// Gems are given random values (these values are logged for TA testing purposes only)
+
+$('#start-button').on('click', function () {
+  userScore = 0
   gemNumbers = [];
-  randomScore = Math.ceil( (Math.random() * 100) + 18)
+  randomScore = Math.ceil((Math.random() * 100) + 18)
+  $('#status').text('').removeClass("status-win", "status-lose");
+  $(this).text("Play Again")
   $('#target-score').text(randomScore);
+  $('#user-score').text(userScore);
   for (var i = 1; i < 5; i++) {
     // Ensure every button has a unique number 2-13 since if the #1 is picked, the user can win too easily.
-    var randomGemNumber = Math.floor( (Math.random() * 12) + 2) 
+    var randomGemNumber = Math.floor((Math.random() * 12) + 2)
     if (gemNumbers.indexOf(randomGemNumber) === -1) {
       gemNumbers.push(randomGemNumber)
     } else {
       i--;
-    } 
-    $('#gem-' + i).val(gemNumbers[i-1])
+    }
+    $('#gem-' + i).val(gemNumbers[i - 1]).removeClass('fade');
+    // The values for the gems are being logged so the Win condition can be tested.  It's not easy to win without the log!
+    console.log("Gem #" + i + " has the value set to: " + $('#gem-' + i).val());
   }
 });
 
 // On click of a gem, add the value of that gem to the user's total
-for(var i=0; i <= 4; i++){
-  $('#gem-' + i).on('click', function(){
-    if ($(this).val()) {
+for (var i = 0; i <= 4; i++) {
+  $('#gem-' + i).on('click', function () {
+    if ($(this).val()) { //make sure the gem has been given a value before trying to add it
       userScore += parseInt($(this).val());
       $('#user-score').text(userScore);
       evaluateScore();
@@ -34,23 +44,26 @@ for(var i=0; i <= 4; i++){
 }
 
 // Determine if Score Matches
-  function evaluateScore(){
-   if (userScore === randomScore){
+function evaluateScore() {
+  if (userScore === randomScore) {
     wins += 1;
     $('#wins').text('Wins: ' + wins);
-    $('#status').text('Congrats, You Won!').attr("id","status-win")
+    $('#status').text('Congrats, You Won!').attr("class", "status-win")
+    gameOver()
 
-   } else if (userScore > randomScore){
-    losses +=1;
+  } else if (userScore > randomScore) {
+    losses += 1;
     $('#losses').text('Losses: ' + losses);
-    $('#status').text('You Went Over.  Sorry, You Lost!').attr("id","status-lose")
-    } else {
-     console.log("Keep going!");
-   }
+    $('#status').text('You Went Over.  Sorry, You Lost!').attr("class", "status-lose")
+    gameOver()
+  } else {
+    console.log("Keep going!");
   }
+}
 
-  //To-Do
-  // 1. Change Start Button to 'Play Again' after initial click of start button
-  // 2. Clear all variables on 'Play Again' - reset function
-  // 3. Clean up presentation
-  // 4. Add logging of numbers so 'win' scenario can be tested.
+// Set all the image values to '' so the user can't keep playing
+function gameOver() {
+  for (var i = 1; i < 5; i++) {
+    $('#gem-' + i).val('').addClass('fade');
+  }
+}
